4aa5fe60145689012dba89e6a9e5024a9eb18bf4
JunkDaCoder <junkdudeofficial@gmail.com>
2017-04-22 16:53:23 -0700

    Add files via upload

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,7 +75,7 @@ namespace pocketmine {
 
 	const VERSION = "1.6.2dev";
 	const API_VERSION = "3.0.0-ALPHA4";
-	const CODENAME = "Aurora";
+	const CODENAME = "Unleashed";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
